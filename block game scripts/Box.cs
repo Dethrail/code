bc0c6251c89c17c7f772b6bd6a841c0dc42d21c5
Dethrail <max.od.ua@gmail.com>
2019-11-18 08:13:22 +0200

    init commit

---
+++ block game scripts/Box.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    private const float RecordTime = 3f;
+    private BoxState _initialState;
+    private Rigidbody _rb;
+
+    private List<BoxState> _boxStates = new List<BoxState>();
+    private bool _record;
+    private float _recordTimer;
+    private float _restoreTimer;
+    private float _timer;
+    private float _lerpValue;
+    private bool _restorePath;
+    private bool _approach;
+    private int _currentIndex;
+
+    private Quaternion _nextRotation;
+    private Vector3 _nextPosition;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    public void StoreInitialState()
+    {
+        _initialState = new BoxState(transform);
+    }
+
+    public void Freeze()
+    {
+        StopBoxFloating();
+        _rb.constraints = RigidbodyConstraints.FreezeAll;
+        _restorePath = true;
+        _restoreTimer = -0.15f;
+        _currentIndex++;
+    }
+
+    public void StopBoxFloating()
+    {
+        _rb.isKinematic = true;
+        _rb.useGravity = false;
+        _rb.velocity = Vector3.zero;
+        _rb.angularVelocity = Vector3.zero;
+    }
+
+    public void StartRecordingPath()
+    {
+        _record = true;
+        _boxStates.Clear();
+        _boxStates.Add(_initialState);
+    }
+
+    private void Update()
+    {
+        if (_record)
+        {
+            RecordPath();
+        }
+
+        if (_restorePath)
+        {
+            RestorePath();
+        }
+    }
+
+    private void RestorePath()
+    {
+        if (_restoreTimer <= RecordTime)
+        {
+            if (_timer >= 1 / 30f - (_currentIndex/3f))
+            {
+                if (_currentIndex >= 0)
+                {
+                    if (_currentIndex >= _boxStates.Count)
+                    {
+                        _currentIndex = _boxStates.Count - 1;
+                    }
+
+                    _nextPosition = _boxStates[_currentIndex].position;
+                    _nextRotation = _boxStates[_currentIndex].rotation;
+                }
+
+                _approach = true;
+                _lerpValue = 0;
+                _timer = 0;
+                _currentIndex--;
+            }
+
+            _restoreTimer += Time.deltaTime;
+            _timer += Time.deltaTime;
+            _lerpValue += Time.deltaTime;
+
+            if (_approach)
+            {
+                transform.position = Vector3.Lerp(transform.position, _nextPosition, _lerpValue);
+                transform.rotation = Quaternion.Slerp(transform.rotation, _nextRotation, _lerpValue);
+            }
+        }
+        else
+        {
+            _rb.velocity = Vector3.zero;
+            _rb.angularVelocity = Vector3.zero;
+            _rb.constraints = RigidbodyConstraints.None;
+            _rb.isKinematic = false;
+            _restoreTimer = 0;
+            _restorePath = false;
+            transform.position = _boxStates[0].position;
+            transform.rotation = _boxStates[0].rotation;
+        }
+    }
+
+    private void RecordPath()
+    {
+        if (_recordTimer <= RecordTime)
+        {
+            if (_timer >= 1 / 10f)
+            {
+                _timer = 0;
+                _boxStates.Add(new BoxState(transform));
+                _currentIndex++;
+            }
+
+            _recordTimer += Time.deltaTime;
+            _timer += Time.deltaTime;
+        }
+        else
+        {
+            _recordTimer = 0;
+            _currentIndex--;
+            _record = false;
+            StopBoxFloating();
+        }
+    }
+}
---
+++ block game scripts/Wall.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    private const float DelayBeforeNewFigure = 2f;
+    public List<WallSeed> Seeds;
+    public List<Box> Boxes;
+    public BoxCollider gridStopperCollider;
+    public Rigidbody epicenter;
+    private readonly List<Figure> _figures = new List<Figure>();
+    private readonly List<Figure> _figuresForCleanUp = new List<Figure>();
+
+    private Vector3 _storedAnchorPosition;
+    private List<WallSeed> _storedSeeds;
+
+    private void Awake()
+    {
+        _storedSeeds = new List<WallSeed>(Seeds);
+    }
+
+    private void Start()
+    {
+        Boxes = new List<Box>(GetComponentsInChildren<WallBox>());
+        foreach (Box box in Boxes)
+        {
+            box.StoreInitialState();
+        }
+    }
+
+    private void CutMatchedFigures()
+    {
+        for (var i = _figures.Count - 1; i >= 0; i--)
+        {
+            if (_figures[i].inGrid)
+            {
+                _figuresForCleanUp.Add(_figures[i]);
+                _figures.Remove(_figures[i]);
+            }
+        }
+        for (var i = _storedSeeds.Count - 1; i >= 0; i--)
+        {
+            if (_storedSeeds[i].TargetFigure.inGrid)
+            {
+                _storedSeeds.Remove(_storedSeeds[i]);
+            }
+        }
+        Seeds = new List<WallSeed>(_storedSeeds);
+    }
+
+    private void DissolveUnmatchedBoxes()
+    {
+        for (var i = _figures.Count - 1; i >= 0; i--)
+        {
+            DissolveTimer[] dissolveTimers = _figures[i].transform.GetComponentsInChildren<DissolveTimer>();
+            foreach (DissolveTimer dissolveTimer in dissolveTimers)
+            {
+                // dissolve timer will destroy box, detach it from figure
+                dissolveTimer.enabled = true;
+                dissolveTimer.transform.parent = null;
+            }
+
+            Destroy(_figures[i].gameObject);
+        }
+    }
+
+    private void FreezeBoxes()
+    {
+        foreach (Box box in Boxes)
+        {
+            box.Freeze();
+        }
+    }
+
+    public IEnumerator Reset()
+    {
+        CutMatchedFigures();
+        DissolveUnmatchedBoxes();
+
+        _figures.Clear();
+        FreezeBoxes();
+
+        yield return new WaitForSeconds(DelayBeforeNewFigure);
+
+        CreateFigures(_storedAnchorPosition);
+    }
+
+    private bool HasSeed()
+    {
+        return Seeds.Count != 0;
+    }
+
+    private Figure CreateFigure(Vector3 anchorPosition)
+    {
+        if (!HasSeed())
+        {
+            return null;
+        }
+
+
+        var seed = Seeds[0];
+        Seeds.Remove(seed);
+        Figure figure = FigureFactory.CreateFigure(seed.type, seed, anchorPosition.z);
+        return figure;
+    }
+
+    public void CreateFigures(Vector3 anchorPosition)
+    {
+        _storedAnchorPosition = anchorPosition;
+        if (_figures.Count != 0)
+        {
+            return;
+        }
+
+        while (HasSeed())
+        {
+            _figures.Add(CreateFigure(anchorPosition));
+        }
+    }
+
+    public void Clear()
+    {
+        for (int i = Seeds.Count - 1; i >= 0; --i)
+        {
+            Destroy(Seeds[i].gameObject);
+            Seeds.Remove(Seeds[i]);
+        }
+
+        for (int i = _figures.Count - 1; i >= 0; --i)
+        {
+            Destroy(_figures[i].gameObject);
+            _figures.Remove(_figures[i]);
+        }
+
+        for (int i = _figuresForCleanUp.Count - 1; i >= 0; --i)
+        {
+            Destroy(_figuresForCleanUp[i].gameObject);
+            _figuresForCleanUp.Remove(_figuresForCleanUp[i]);
+        }
+    }
+
+    public bool IsFilled()
+    {
+        return _figures.Count((f) => f.inGrid) == _figures.Count;
+    }
+
+    public void StartRecord()
+    {
+        foreach (Box box in Boxes)
+        {
+            box.StartRecordingPath();
+        }
+
+        // assign wall box to mathed figure boxes and record theirs path
+        for (var i = _storedSeeds.Count - 1; i >= 0; i--)
+        {
+            if (_storedSeeds[i].TargetFigure.inGrid)
+            {
+                foreach (Box box in _storedSeeds[i].TargetFigure.Boxes)
+                {
+                    WallBox wb = box.gameObject.AddComponent<WallBox>();
+                    wb.StoreInitialState();
+                    wb.StartRecordingPath();
+                    Boxes.Add(wb);
+                }
+            }
+        }
+    }
+}
